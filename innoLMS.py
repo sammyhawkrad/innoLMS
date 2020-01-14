c3cf459f489be5580803831578255349144e5923
Samuel Darkwah Manu <manusam14@yahoo.com>
2020-01-14 15:43:50 +0000

    Clean up code

--- innoLMS.py
+++ innoLMS.py
@@ -1,9 +1,6 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import tkinter
->>> window = tkinter.Tk()
->>> window.title("getINNOtized Library Management System")
-''
->>> label = tkinter.Label(window, text = "getINNOtized Library Management System").pack()
->>> window.mainloop()
+import tkinter
+window = tkinter.Tk()
+window.title("getINNOtized Library Management System")
+label = tkinter.Label(window, text = "getINNOtized Library Management System").pack()
+window.mainloop()
 
